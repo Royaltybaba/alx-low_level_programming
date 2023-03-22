66b6355b813f87fdf2f23096b30a2493a35a06d3
Royaltybaba <adedayoomtohso@gmail.com>
2023-03-22 07:19:58 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ *
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'a' ; c++)
+	_putchar(c);
+	_putchar('\n');
+}
