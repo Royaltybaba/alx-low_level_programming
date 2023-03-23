dcec45dc95d687068110f3d7253bfbbdd70ae229
Royaltybaba <adedayoomtohso@gmail.com>
2023-03-23 10:42:06 -0700

    multiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiples of two integers
+ * @x: first miltiplier
+ * @y: second multiplier
+ * Return: result of multiplication  of x and y
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
